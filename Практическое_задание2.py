fea792b08f4c473b8386e9866d55196e01e36fa7
KhadzhievaMariyam <khadzhiieva2000@mail.ru>
2024-11-18 00:22:10 +0300

    Update and rename Практическое_задание2.py to module_1_2.py

--- Практическое_задание2.py
+++
@@ -1,5 +0,0 @@
-completed_tasks = 12
-hours_spent = 1.5
-name = "Python"
-average_time = (hours_spent/completed_tasks)
-print ('Курс:', name, ', ' 'всего задач:', completed_tasks, ', ' 'затрачено часов:', hours_spent, ', ' 'среднее время выполнения ', average_time, sep="")
